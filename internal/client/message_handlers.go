7e90e73987d02726483d9b22e3f9a09c458957ee
vagif <vagifmammadaliyev@outlook.com>
2026-05-31 03:08:13 +0400

    Client Side Error handling (#18)
    * feat: terminal move cursor right by N columns
    
    * feat: proper user hud
    
    - client side message handlers can now interact with hud
    - hud is capable of rendering user related info + debug related info
    
    * fix: imports fixed

--- internal/client/message_handlers.go
+++ internal/client/message_handlers.go
@@ -5,41 +5,47 @@ import (
 	"io"
 
 	"github.com/VagifMammadaliyev/juicy-snake-term/internal/engine"
-	"github.com/VagifMammadaliyev/juicy-snake-term/internal/messages"
-	"github.com/VagifMammadaliyev/juicy-snake-term/internal/terminal"
 )
 
-func init() {
-	messages.RegisterHandler(messages.MsgTypeAreaUpdate, msgHandler(areaUpdateHandler))
+type msgHandler struct {
+	handleFunc func(io.Writer, *hud, []byte) error
+	hud        *hud
 }
 
-type msgHandler func(io.Writer, []byte) error
-
-func (h msgHandler) Handle(w io.Writer, data []byte) error {
-	return h(w, data)
+func newMsgHandler(handleFunc func(
+	io.Writer,
+	*hud,
+	[]byte,
+) error, hud *hud) *msgHandler {
+	return &msgHandler{
+		handleFunc: handleFunc,
+		hud:        hud,
+	}
 }
 
-func areaUpdateHandler(w io.Writer, buffer []byte) error {
+func (m *msgHandler) Handle(w io.Writer, data []byte) error {
+	return m.handleFunc(w, m.hud, data)
+}
+
+func areaUpdateHandler(w io.Writer, h *hud, buffer []byte) error {
 	area, playerSnakeHead, err := engine.NewAreaFromBytes(buffer)
 	if err != nil {
 		return fmt.Errorf("can't decode server update: %v", err)
 	}
 
-	for _, b := range area.Bounders {
-		if b == nil {
-			fmt.Println("received nil for bounder")
-		}
-	}
 	camera := engine.NewCenteredCamera(playerSnakeHead)
-
 	area.RenderForCamera(w, camera)
 
-	terminal.MoveCursor(w, area.Rows+1, 0)
-	fmt.Fprintf(w, "Bytes received: %5d\n\r", len(buffer))
-	fmt.Fprintf(w, "Area size: %2dx%2d\n\r", area.Cols, area.Rows)
-	fmt.Fprintf(w, "Bounders: %4d\n\r", len(area.Bounders))
-	fmt.Fprintf(w, "Per bounder: %4.2f bytes\n\r", float64(len(buffer))/float64(len(area.Bounders)))
-	fmt.Fprintf(w, "Player snake head: %2d,%2d\n\r", playerSnakeHead.X, playerSnakeHead.Y)
+	di := debugInfo{
+		bytesReceived:   len(buffer),
+		decodedAreaCols: int(area.Cols),
+		decodedAreaRows: int(area.Rows),
+		boundersCount:   len(area.Bounders),
+	}
+	h.debug = di
+	h.playerPosition = playerSnakeHead
+	h.playerScore = -1
+	h.Point = engine.Point{X: 0, Y: area.Rows + 1}
 
 	return nil
 }
